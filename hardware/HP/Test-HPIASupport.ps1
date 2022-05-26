11a38dc9ff714e00fef0d0bbb37d35a1cf95160b
gwblok <garywblok@gmail.com>
2022-05-26 11:50:48 -0500

    Create Test-HPIASupport.ps1

---
+++ hardware/HP/Test-HPIASupport.ps1
@@ -0,0 +1,14 @@
+function Test-HPIASupport {
+    $CabPath = "$env:TEMP\platformList.cab"
+    $XMLPath = "$env:TEMP\platformList.xml"
+    $PlatformListCabURL = "https://hpia.hpcloud.hp.com/ref/platformList.cab"
+    Invoke-WebRequest -Uri $PlatformListCabURL -OutFile $CabPath -UseBasicParsing
+    $Expand = expand $CabPath $XMLPath
+    [xml]$XML = Get-Content $XMLPath
+    $Platforms = $XML.ImagePal.Platform.SystemID
+    $MachinePlatform = (Get-CimInstance -Namespace root/cimv2 -ClassName Win32_BaseBoard).Product
+    if ($MachinePlatform -in $Platforms){$HPIASupport = $true}
+    else {$HPIASupport = $false}
+
+    return $HPIASupport
+    }
