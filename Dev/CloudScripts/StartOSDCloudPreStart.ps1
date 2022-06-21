d5a93eabf801d3e1164b252b2906878856b228d5
gwblok <garywblok@gmail.com>
2022-06-21 16:05:56 -0500

    Update StartOSDCloudPreStart.ps1

--- Dev/CloudScripts/StartOSDCloudPreStart.ps1
+++ Dev/CloudScripts/StartOSDCloudPreStart.ps1
@@ -1,3 +1,4 @@
+#powershell Invoke-Expression -Command (Invoke-RestMethod -Uri https://raw.githubusercontent.com/gwblok/garytown/master/Dev/CloudScripts/StartOSDCloudPreStart.ps1)
 Invoke-Expression (Invoke-RestMethod 'sandbox.osdcloud.com')
 $Global:MyOSDCloud = [ordered]@{
         Restart = [bool]$False
