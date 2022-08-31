9d53d365f65a03f871a99cf526f1508e91bc0d9c
gwblok <garywblok@gmail.com>
2022-08-31 16:40:55 -0500

    Update Run-WindowsUpdates.ps1

--- RunScripts/Run-WindowsUpdates.ps1
+++ RunScripts/Run-WindowsUpdates.ps1
@@ -1,4 +1,38 @@
-# Gary Blok - GARYTOWN.COM
+<# Control Windows Update via PowerShell
+Gary Blok - GARYTOWN.COM
+NOTE: I'm using this in a RUN SCRIPT, so I hav the Parameters set to STRING, and in the RUN SCRIPT, I Create a list of options (TRUE & FALSE).
+In a normal script, you wouldn't do this... so modify for your deployment method.
+
+THis was also intended to be used with ConfigMgr, if you're not, feel free to remove the $CMReboot & Corrisponding Function
+
+#>
+[CmdletBinding()]
+    Param (
+		    [Parameter(Mandatory=$true)][string]$CMReboot = "FALSE",
+            [Parameter(Mandatory=$true)][string]$RestartNow = "FALSE",
+            [Parameter(Mandatory=$true)][string]$Install = "FALSE"
+	    )
+
+Function Restart-ComputerCM {
+    if (Test-Path -Path "C:\windows\ccm\CcmRestart.exe"){
+
+        $time = [DateTimeOffset]::Now.ToUnixTimeSeconds()
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'RebootBy' -Value $time -PropertyType QWord -Force -ea SilentlyContinue;
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'RebootValueInUTC' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'NotifyUI' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'HardReboot' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'OverrideRebootWindowTime' -Value 0 -PropertyType QWord -Force -ea SilentlyContinue;
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'OverrideRebootWindow' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'PreferredRebootWindowTypes' -Value @("4") -PropertyType MultiString -Force -ea SilentlyContinue;
+        $Null = New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -Name 'GraceSeconds' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+
+        $CCMRestart = start-process -FilePath C:\windows\ccm\CcmRestart.exe -NoNewWindow -PassThru
+    }
+    else {
+        Write-Output "No CM Client Found"
+    }
+}
+
 $Results = @(
 @{ ResultCode = '0'; Meaning = "Not Started"}
 @{ ResultCode = '1'; Meaning = "In Progress"}
@@ -17,17 +51,29 @@ $WUUpdates=New-Object -ComObject Microsoft.Update.UpdateColl
 }
 
 if ($WUUpdates.Count -ge 1){
-    $WUInstaller.ForceQuiet=$true
-    $WUInstaller.Updates=$WUUpdates
-    $WUDownloader.Updates=$WUUpdates
-    write-host "Downloading " $WUDownloader.Updates.count "Updates"
-    foreach ($update in $WUInstaller.Updates){Write-Host "$($update.Title)"}
-    $WUDownloader.Download()
-    write-host "Installing " $WUInstaller.Updates.count "Updates"
-    $Install = $WUInstaller.Install()
-    $ResultMeaning = ($Results | Where-Object {$_.ResultCode -eq $Install.ResultCode}).Meaning
-    Write-Output $ResultMeaning
+    if ($Install -eq "TRUE"){
+        $WUInstaller.ForceQuiet=$true
+        $WUInstaller.Updates=$WUUpdates
+        $WUDownloader.Updates=$WUUpdates
+        write-host "Downloading " $WUDownloader.Updates.count "Updates"
+        foreach ($update in $WUInstaller.Updates){Write-Host "$($update.Title)"}
+        $WUDownloader.Download()
+        write-host "Installing " $WUInstaller.Updates.count "Updates"
+        $Install = $WUInstaller.Install()
+        $ResultMeaning = ($Results | Where-Object {$_.ResultCode -eq $Install.ResultCode}).Meaning
+        Write-Output $ResultMeaning
+        if ($Install.RebootRequired -eq $true){
+            if ($CMReboot -eq "TRUE"){Restart-ComputerCM}
+            if ($RestartNow -eq "TRUE") {Restart-Computer -Force}
+        }
+    }
+    else
+        {
+        Write-Output "Available Updates:"
+        foreach ($update in $WUInstaller.Updates){Write-Host "$($update.Title)"}
+     }
 } 
 else {
     write-host "No updates detected"
 }
+
